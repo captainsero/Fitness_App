6ee3190ec1835ebb985c225b1c18b0ec534cd48e
AbdullahAtef1 <atefabdullah063@gmail.com>
2026-07-25 21:30:23 +0300

    + workouts feat

--- lib/core/constants/api_constants/api_endpoints.dart
+++ lib/core/constants/api_constants/api_endpoints.dart
@@ -8,4 +8,8 @@ abstract class ApiEndpoints {
   static const String forgotPassword = '$baseUrl/auth/forgotPassword';
   static const String verifyResetCode = '$baseUrl/auth/verifyResetCode';
   static const String resetPassword = '$baseUrl/auth/resetPassword';
+  static const String allMuscles = '/muscles';
+  static const String randomPrimeMoverMuscles = '/muscles/random';
+  static const String muscleGroups = '/muscles';
+  static const String musclesByGroup = '/musclesGroup/by-muscle-group';
 }
